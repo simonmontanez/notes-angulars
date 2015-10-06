bc0ac1ef34c9436152d89e3d16ea3bfa9c23b0bf
simonmontanez <simonmontanez@seven4n.com>
2015-10-05 20:18:16 -0500

    Página Inicial

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/iniciando', {
+        templateUrl: 'views/iniciando.html',
+        controller: 'InciandoCtrl',
+        controllerAs: 'iniciando'
+      })
       .otherwise({
         redirectTo: '/'
       });
