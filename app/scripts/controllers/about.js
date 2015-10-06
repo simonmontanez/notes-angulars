717a88bf2c53bbb03e684509c6dca3b869ccc389
simonmontanez <simonmontanez@seven4n.com>
2015-10-05 20:11:59 -0500

    Inicial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name iniciandoAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the iniciandoAngularApp
+ */
+angular.module('iniciandoAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name iniciandoAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the iniciandoAngularApp
+ */
+angular.module('iniciandoAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
